b881a3a98ff0ad62512788bc17bf61861a626e08
KlochkovDimka <KlochkovDimka97@yandex.ru>
2024-11-12 09:46:12 +0300

    New

---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.example.dto.UserAuthentication;
+import org.json.JSONObject;
+import org.testng.annotations.Test;
+
+public class MainTest {
+
+    @Test
+    public void authentication() {
+        RestAssured.baseURI = "https://aqa-api.javacode.ru/api/auth/login";
+        UserAuthentication user = new UserAuthentication("klochkov_dmitriy", "\\lWX${6vY_VQ[Is");
+
+        JSONObject userReg = new JSONObject();
+        userReg.put("username", "klochkov_dmitriy");
+        userReg.put("password", "\\lWX${6vY_VQ[Is");
+
+        Response response = RestAssured
+                .given()
+                    .log().all()
+                    .header("Content/type", "application/json")
+                    .body(userReg.toString())
+                .when().post();
+
+        response.then()
+                .statusCode(200);
+
+
+    }
+}
---
+++ src/test/java/org/example/dto/UserAuthentication.java
@@ -0,0 +1,12 @@
+package org.example.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserAuthentication {
+
+    private String username;
+    private String password;
+}
